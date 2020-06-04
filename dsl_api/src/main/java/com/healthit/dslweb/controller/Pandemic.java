dd102ba3421c678d59b13fef2ad060d7a1be50bc
Duncan Ndiithi <duncanndiithi@yahoo.com>
2020-06-04 17:27:01 +0300

    covid whole data set

--- dsl_api/src/main/java/com/healthit/dslweb/controller/Pandemic.java
+++ dsl_api/src/main/java/com/healthit/dslweb/controller/Pandemic.java
@@ -3,8 +3,11 @@ package com.healthit.dslweb.controller;
 import com.healthit.dslservice.DslException;
 import com.healthit.dslservice.dao.LocationDao;
 import com.healthit.dslservice.dao.PandemicDao;
+import com.healthit.dslservice.dao.SurveyDao;
 import com.healthit.dslservice.dto.adminstrationlevel.Constituency;
 import com.healthit.dslservice.message.Message;
+import com.healthit.dslservice.message.MessageType;
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
@@ -12,9 +15,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
@@ -40,6 +47,60 @@ public class Pandemic {
             return new ResponseEntity<String>("Unknown request", HttpStatus.BAD_REQUEST);
         }
 
+    }
+
+    @CrossOrigin
+    @ResponseBody
+    @RequestMapping(value = "pandemics/{pandemicSource}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getIndicatorValue(
+            @RequestParam(value = "id", required = false) String indicatorId,
+            @RequestParam(value = "orgId", required = false) String orgId,
+            @RequestParam(value = "start_date", required = false) String startDate,
+            @RequestParam(value = "end_date", required = false) String endDate,
+            @PathVariable(value = "pandemicSource", required = true) String pandemicSource
+    ) {
+        
+        log.info("parameters ===>");
+        log.info(indicatorId);
+        log.info(orgId);
+        log.info(startDate);
+        log.info(endDate);
+        log.info(pandemicSource);
+        log.info("===========>");
+        
+        PandemicDao pandemicDao = new PandemicDao();
+        try {
+            if (startDate != null || endDate != null) {
+
+                try {
+                    if (startDate != null) {
+                        Date _startDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDate);
+                    }
+                } catch (ParseException ex) {
+                    Message msg = new Message();
+                    msg.setMesageContent("Wrong start date format. Should be yyyy-MM-dd");
+                    msg.setMessageType(MessageType.YEAR_FORMAT_ERROR);
+                    throw new DslException(msg);
+                }
+
+                try {
+                    if (endDate != null) {
+                        Date _endDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDate);
+                    }
+                } catch (ParseException ex) {
+                    Message msg = new Message();
+                    msg.setMesageContent("Wronf end date format. Should be yyyy-MM-dd");
+                    msg.setMessageType(MessageType.YEAR_FORMAT_ERROR);
+                    throw new DslException(msg);
+                }
+
+            }
+
+            return new ResponseEntity(pandemicDao.getPandemicData(pandemicSource, indicatorId, orgId, startDate, endDate), HttpStatus.OK);
+
+        } catch (DslException ex) {
+            return new ResponseEntity<Message>(ex.getMsg(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- dsl_service_parent_project/dsl_api_implementation/src/main/java/com/healthit/dslservice/dao/PandemicDao.java
+++ dsl_service_parent_project/dsl_api_implementation/src/main/java/com/healthit/dslservice/dao/PandemicDao.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.Element;
 import org.apache.log4j.Logger;
@@ -27,11 +28,18 @@ public class PandemicDao {
     Cache cache = DslCache.getCache();
 
     private String selectPandemicIndicators = "Select pandemic_indicator_id as id, name FROM dim_pandemic_indicator";
+    private String selectPandemicDataSql = "select fp.indicator_id as indicatorId,dim_pindc.name as indicator_name, dimp.date as date, \n"
+            + "porg.name as orgUnit, fp.orgunit_id as orgId ,porg.latitude as org_latitude, porg.longitude as org_longitude"
+            + ",fp.value as value from fact_pandemic fp \n"
+            + "inner join dim_pandemic_period dimp on dimp.pandemic_period_id=fp.period_id\n"
+            + "inner join pandemic_comm_org_unit porg on porg.pan_org_id =fp.orgunit_id\n"
+            + "inner join dim_pandemic_indicator dim_pindc on dim_pindc.pandemic_indicator_id =fp.indicator_id";
 
     /**
      * Gets a list of pandemics
+     *
      * @return List<Map> A list of pandemics and their associated indciators
-     * @throws DslException 
+     * @throws DslException
      */
     public List<Map> getPandemics() throws DslException {
         List<Map> pandemicList = new ArrayList();
@@ -41,7 +49,7 @@ public class PandemicDao {
         pandemics.put("name", "covid 19");
         pandemics.put("initial_report_date", "2020-03-17");
 
-        Element ele = cache.get(CacheKeys.indicatorGroup);
+        Element ele = cache.get(CacheKeys.pandemicsList);
 
         if (ele == null) {
             Database db = new Database();
@@ -60,7 +68,11 @@ public class PandemicDao {
             } catch (SQLException ex) {
                 log.error(ex);
             } finally {
-                db.CloseConnection();
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    log.error(ex);
+                }
             }
         } else {
             long startTime = System.nanoTime();
@@ -69,5 +81,86 @@ public class PandemicDao {
             log.info("Time taken to fetch data from cache " + (endTime - startTime) / 1000000);
         }
         return pandemicList;
+    }
+
+    public Map<String, Map> getPandemicData(String pandemicSource, String indicatorId, String orgId, String startDate, String endDate) throws DslException {
+        String cacheName = pandemicSource + indicatorId + orgId + startDate + endDate;
+        Element ele = cache.get(cacheName);
+        Map<String, Map> result = new HashMap();
+        Map<String, Object> dictionary = new HashMap();
+        Map<Integer, List<Map>> data = new HashMap();
+
+        List<Map<String, Object>> orgUnitsList = new ArrayList();
+        List<Integer> addedOrgUnits = new ArrayList();
+        List<PandemicIndicator> indicatorList = new ArrayList();
+        List<Map<String, String>> period = new ArrayList();
+
+        if (ele == null) {
+            Database db = new Database();
+            ResultSet rs = db.executeQuery(selectPandemicDataSql);
+            log.info("Fetching indicator groups");
+            try {
+                while (rs.next()) {
+                    Map dataUnit = new HashMap(); // keys: period, ouid, value
+                    int dbIndicatorId = rs.getInt("indicatorId");
+                    int dbOrgId = rs.getInt("orgId");
+                    String dbOrgUnitName = rs.getString("orgUnit");
+                    String dbIndicatorName = rs.getString("indicator_name");
+                    if (data.containsKey(dbIndicatorId)) {
+
+                        dataUnit.put("period", rs.getDate("date"));
+                        dataUnit.put("ou", dbOrgId);
+                        dataUnit.put("value", rs.getInt("value"));
+                        data.get(dbIndicatorId).add(dataUnit);
+
+                    } else {
+                        List<Map> dataList = new ArrayList();
+                        PandemicIndicator pIndic = new PandemicIndicator();
+                        pIndic.setId(dbIndicatorId);
+                        pIndic.setName(dbIndicatorName);
+                        indicatorList.add(pIndic);
+
+                        dataUnit.put("period", rs.getDate("date"));
+                        dataUnit.put("ou", rs.getInt("orgId"));
+                        dataUnit.put("value", rs.getInt("value"));
+                        dataList.add(dataUnit);
+                        data.put(dbIndicatorId, dataList);
+
+                    }
+                    if (!addedOrgUnits.contains(dbOrgId)) {
+                        Map<String, Object> orgMapUnit = new HashMap();
+                        orgMapUnit.put("id", dbOrgId);
+                        orgMapUnit.put("name", dbOrgUnitName);
+                        orgMapUnit.put("latitude", rs.getString("org_latitude"));
+                        orgMapUnit.put("longitude", rs.getString("org_longitude"));
+                        orgUnitsList.add(orgMapUnit);
+                        addedOrgUnits.add(dbOrgId);
+                    }
+
+                }
+
+                dictionary.put("orgunits", orgUnitsList);
+                dictionary.put("indicators", indicatorList);
+
+                result.put("data", data);
+                result.put("dictionary", dictionary);
+
+                cache.put(new Element(cacheName, result));
+            } catch (SQLException ex) {
+                log.error(ex);
+            } finally {
+                try {
+                    rs.close();
+                } catch (SQLException ex) {
+                    log.error(ex);
+                }
+            }
+        } else {
+            long startTime = System.nanoTime();
+            result = (Map<String, Map>) ele.getObjectValue();
+            long endTime = System.nanoTime();
+            log.info("Time taken to fetch data from cache " + (endTime - startTime) / 1000000);
+        }
+        return result;
     }
 }
